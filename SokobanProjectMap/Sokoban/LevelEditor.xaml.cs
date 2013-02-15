ec572862932c7971367a983a43829d88173943f7
Johan Beekers <johan.beekers@gmail.com>
2013-02-16 00:59:40 +0100

    Level Editor grotendeels werkend.

--- SokobanProjectMap/Sokoban/LevelEditor.xaml.cs
+++ SokobanProjectMap/Sokoban/LevelEditor.xaml.cs
@@ -20,21 +20,258 @@ namespace Sokoban
     /// </summary>
     public partial class LevelEditor : UserControl
     {
-        ModelLevel levelModel;
+        private List<List<Tile>> tiles = new List<List<Tile>>();
+        private ModelLevel levelModel;
+        private static int minGridWidth = 5, minGridHeight = 5, maxGridWidth = 9, maxGridHeight = 9;
 
         public LevelEditor(ModelLevel levelModel)
         {
             InitializeComponent();
-            this.levelModel = levelModel;
 
-            Binding mapsListBinding = new Binding("maps");
-            mapsListBinding.Source = levelModel;
-            mapsListBox.SetBinding(ListBox.ItemsSourceProperty, mapsListBinding);
+            //Fill the list with existing levels
+            this.levelModel = levelModel;
+            mapsListBox.ItemsSource = levelModel.Maps;
+
+            //Fill the list with level tiles.
+            tilesListBox.Items.Add(new Wall());
+            tilesListBox.Items.Add(new Floor());
+            tilesListBox.Items.Add(new Target());
+            tilesListBox.Items.Add(new Box());
+            tilesListBox.Items.Add(new Forklift());
+            tilesListBox.SelectedIndex = 0;
+
+            initGrid();
         }
 
         private void Back_Button_Click(object sender, RoutedEventArgs e)
         {
             this.Visibility = Visibility.Collapsed;
+        }
+
+        private void initGrid()
+        {
+            int width = levelGrid.ColumnDefinitions.Count();
+            int height = levelGrid.RowDefinitions.Count();
+
+            for (int y = 0; y < height; y++)
+            {
+                List<Tile> row = new List<Tile>();
+                for (int x = 0; x < width; x++)
+                {
+                    if (x == 0 || y == 0 || x == width - 1 || y == height - 1)
+                    {
+                        Wall wall = new Wall();
+                        wall.SetValue(Grid.ColumnProperty, x);
+                        wall.SetValue(Grid.RowProperty, y);
+
+                        row.Add(wall);
+                        levelGrid.Children.Add(wall);
+                    }
+                    else
+                    {
+                        Floor floor = new Floor();
+                        floor.SetValue(Grid.ColumnProperty, x);
+                        floor.SetValue(Grid.RowProperty, y);
+
+                        row.Add(floor);
+                        levelGrid.Children.Add(floor);
+                    }
+                }
+                tiles.Add(row);
+            }
+        }
+
+        private void verticalPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (levelGrid.RowDefinitions.Count() >= maxGridHeight)
+            {
+                return;
+            }
+
+            //Add a new row to the grid.
+            RowDefinition gridRow = new RowDefinition();
+            gridRow.Height = new GridLength(levelModel.CellSize);
+            levelGrid.RowDefinitions.Add(gridRow);
+
+            //Get the new total width and height. 
+            int width = levelGrid.ColumnDefinitions.Count();
+            int height = levelGrid.RowDefinitions.Count();
+
+            //Move the wall row one down in both the tiles array and grid.
+            foreach (Tile tile in tiles[height - 2])
+            {
+                tile.SetValue(Grid.RowProperty, height - 1);
+            }
+            tiles.Add(tiles[height-2]);
+
+            //Create a new row that will be inserted in the old wall row. 
+            List<Tile> row = new List<Tile>();
+            for (int i = 0; i < width; i++)
+            {
+                //Walls left and right. The rest is floor.
+                if (i == 0 || i == width - 1)
+                {
+                    Wall wall = new Wall();
+                    wall.SetValue(Grid.ColumnProperty, i);
+                    wall.SetValue(Grid.RowProperty, height-2);
+
+                    row.Add(wall);
+                    levelGrid.Children.Add(wall);
+                }
+                else
+                {
+                    Floor floor = new Floor();
+                    floor.SetValue(Grid.ColumnProperty, i);
+                    floor.SetValue(Grid.RowProperty, height-2);
+
+                    row.Add(floor);
+                    levelGrid.Children.Add(floor);
+                }
+            }
+            tiles[height - 2] = row;
+        }
+
+        private void verticalMin_Click(object sender, RoutedEventArgs e)
+        {
+            if (levelGrid.RowDefinitions.Count() <= minGridHeight)
+            {
+                return;
+            }
+
+            //Get the current total width and height. 
+            int width = levelGrid.ColumnDefinitions.Count();
+            int height = levelGrid.RowDefinitions.Count();
+
+            //Remove the row above the wall row from the grid. 
+            foreach (Tile tile in tiles[height - 2])
+            {
+                levelGrid.Children.Remove(tile);
+            }
+            //Move the wall row one row up.
+            foreach (Tile tile in tiles[height - 1])
+            {
+                tile.SetValue(Grid.RowProperty, height-2);
+            }
+            //Move the row in the tiles array and remove the old one.
+            tiles.RemoveAt(height-2);
+            
+            //Remove a row from the grid.
+            levelGrid.RowDefinitions.RemoveAt(levelGrid.RowDefinitions.Count()-1);
+        }
+
+        private void horizontalPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (levelGrid.ColumnDefinitions.Count() >= maxGridWidth)
+            {
+                return;
+            }
+
+            //Add a new column to the grid.
+            ColumnDefinition GridColumn = new ColumnDefinition();
+            GridColumn.Width = new GridLength(levelModel.CellSize);
+            levelGrid.ColumnDefinitions.Add(GridColumn);
+
+            //Get the new total width and height. 
+            int width = levelGrid.ColumnDefinitions.Count();
+            int height = levelGrid.RowDefinitions.Count();
+
+            foreach(List<Tile> row in tiles)
+            {
+                row.Add(row[width-2]);
+                row[width - 1].SetValue(Grid.ColumnProperty, width - 1);
+            }
+
+            //Add new tiles to the new column.
+            for (int i = 0; i < height; i++)
+            {
+                //Walls up and down. The rest is floor.
+                if (i == 0 || i == height - 1)
+                {
+                    Wall wall = new Wall();
+                    wall.SetValue(Grid.ColumnProperty, width - 2);
+                    wall.SetValue(Grid.RowProperty, i);
+
+                    tiles[i][width-2] = wall;
+                    levelGrid.Children.Add(wall);
+                }
+                else
+                {
+                    Floor floor = new Floor();
+                    floor.SetValue(Grid.ColumnProperty, width -2);
+                    floor.SetValue(Grid.RowProperty, i);
+
+                    tiles[i][width - 2] = floor;
+                    levelGrid.Children.Add(floor);
+                }
+            }
+        }
+
+        private void horizontalMin_Click(object sender, RoutedEventArgs e)
+        {
+            if (levelGrid.ColumnDefinitions.Count() <= minGridWidth)
+            {
+                return;
+            }
+
+            //Get the current total width and height. 
+            int width = levelGrid.ColumnDefinitions.Count();
+            int height = levelGrid.RowDefinitions.Count();
+
+            //Move the wall column one right in both grid and array
+            foreach (List<Tile> row in tiles)
+            {
+                levelGrid.Children.Remove(row[width - 2]);
+                row[width - 1].SetValue(Grid.ColumnProperty, width - 2);
+                row.RemoveAt(width - 2);
+            }
+
+            //Remove a column from the grid.
+            levelGrid.ColumnDefinitions.RemoveAt(levelGrid.ColumnDefinitions.Count() - 1);
+        }
+
+        private void saveMap_Click(object sender, RoutedEventArgs e)
+        {
+            MapSaver save = new MapSaver(tiles, fileName.Text);
+        }
+
+        private void levelGrid_MouseDown(Object sender, MouseButtonEventArgs e)
+        {
+            //Er was werkelijk niks te vinden op internet betreft het vinden van een geklikte kolom in een grid. (geen werkende WPF manier tenminste)
+            //Daarom zelf maar een functie geschreven die de muis coordinaten geeft binnen het levelGrid. Die delen door 40 en afronden op geheel getal geeft exact de goede kolom/rij.
+            int mousePositionX = (int)Mouse.GetPosition(levelGrid).X;
+            int mousePositionY = (int)Mouse.GetPosition(levelGrid).Y;
+            int column = mousePositionX / levelModel.CellSize;
+            int row = mousePositionY / levelModel.CellSize;
+            placeTile(column, row);
+        }
+
+        private void placeTile(int x, int y)
+        {
+            Tile newTile;
+            switch (tilesListBox.SelectedItem.ToString())
+            {
+                case "Sokoban.Wall":
+                    newTile = new Wall();
+                    break;
+                case "Sokoban.Target":
+                    newTile = new Target();
+                    break;
+                /*case "Sokoban.Box":
+                    newTile = new Box();
+                    break;
+                case "Sokoban.Forklift":
+                    newTile = new Forklift();
+                    break;*/
+                default:
+                    newTile = new Floor();
+                    break;
+            }
+            newTile.SetValue(Grid.ColumnProperty, x);
+            newTile.SetValue(Grid.RowProperty, y);
+
+            levelGrid.Children.Remove(tiles[y][x]);
+            tiles[y][x] = newTile;
+            levelGrid.Children.Add(tiles[y][x]);
         }
     }
 }
---
+++ SokobanProjectMap/Sokoban/SaveMap.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sokoban
+{
+    class MapSaver
+    {
+        private String file;
+        private List<List<Tile>> tileMap;
+        private String[] stringMap;
+        private int forklifts;
+        private int targets;
+        private int boxes;
+
+        public MapSaver(List<List<Tile>> tileMap, String mapName)
+        {
+            file = "map/" + mapName + ".map";
+            if (File.Exists(file))
+            {
+                MessageBoxResult result = MessageBox.Show("Er bestaat al een map met deze naam. \r\n Wilt u deze overschrijven?", "File exists", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.No)
+                {
+                    return;
+                }
+            }
+
+            this.tileMap = tileMap;
+            stringMap = new String[tileMap.Count()];
+            forklifts = 0;
+            targets = 0;
+            boxes = 0;
+
+            convertToStringMap();
+           /* if (validateMap() != false)
+            {
+                save();
+            }*/
+
+            save();
+        }
+
+        private void convertToStringMap()
+        {
+            int lineNumber = 0;
+            foreach(List<Tile> row in tileMap)
+            {
+                String line = "";
+                foreach(Tile tile in row)
+                {
+                    switch (tile.ToString())
+                    {
+                        case "Sokoban.Wall":
+                            line += "#";
+                            break;
+                        case "Sokoban.Target":
+                            line += "x";
+                            targets++;
+                            break;
+                        case "Sokoban.Box":
+                            line += "o";
+                            boxes++;
+                            break;
+                        case "Forklift":
+                            line += "@";
+                            forklifts++;
+                            break;
+                        default:
+                            line += " ";
+                            break;
+                    }
+                }
+                stringMap[lineNumber] = line;
+                lineNumber++;
+            }
+        }
+
+        private Boolean validateMap()
+        {
+            if(forklifts > 1)
+            {
+                MessageBox.Show("Er mag maar 1 speler op de map voorkomen.", "Map incorrect", MessageBoxButton.OK, MessageBoxImage.Information);
+                return false;
+            }
+            else if(targets == 0)
+            {
+                MessageBox.Show("Er moet minstens 1 doel op de map voorkomen", "Map incorrect", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else if(boxes < targets)
+            {
+                MessageBox.Show("Er moeten minstens evenveel kisten als doelen op de map voorkomen", "Map incorrect", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            return true;
+        }
+
+        private void save()
+        {
+            StreamWriter sw = new StreamWriter(file);
+            foreach (String line in stringMap)
+            {
+                sw.WriteLine(line);
+            }
+            sw.Close();
+        }
+    }
+}
